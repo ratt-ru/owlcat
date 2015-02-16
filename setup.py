1e9172e5ac33b03a65652527cb7848d727979f06
Gijs Molenaar <gijs@pythonic.nl>
2015-02-16 15:55:26 +0200

    install the simms, submodule

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import os
 from distutils.core import setup
 
 setup(name='owlcat',
-      version='1.3.0',
+      version='1.3.3',
       description='miscellaneous utility scripts for manipulating radio interferometry data',
       author='Oleg Smirnov',
       author_email='Oleg Smirnov <osmirnov@gmail.com>',
       url='https://github.com/ska-sa/owlcat',
-      packages=['Owlcat'],
+      packages=['Owlcat', 'Owlcat.simms'],
       requires=['pyfits', 'numpy', 'matplotlib', 'pyrap', 'meqtrees_cattery'],
       scripts=['Owlcat/bin/' + i for i in os.listdir('Owlcat/bin')],
       data_files=[('Owlcat/bin/', ['Owlcat/bin/commands.list'])],
