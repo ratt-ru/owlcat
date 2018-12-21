17ca6c4e61b6efb8f4951aa46e2b98a4ce2bc30d
Gijs Molenaar <gijs@pythonic.nl>
2018-12-21 10:15:37 +0200

    prepare for release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 ]
 
 setup(name='owlcat',
-      version='1.4.3',
+      version='1.5.0',
       description='miscellaneous utility scripts for manipulating radio interferometry data',
       author='Oleg Smirnov',
       author_email='Oleg Smirnov <osmirnov@gmail.com>',
