2be3c537073ba6ff86df474e204465c8a405cb19
Benjamin Hugo <bennahugo@gmail.com>
2022-03-24 12:12:09 +0200

    bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires = [
 ]
 
 setup(name='owlcat',
-      version='1.7.7',
+      version='1.7.8.3',
       python_requires='>=3.6.0',
       description='miscellaneous utility scripts for manipulating radio interferometry data',
       author='Oleg Smirnov',
